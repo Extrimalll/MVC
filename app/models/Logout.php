984929d31368694a5a096aaf4cadc45f845e0627
Владислав <yasyrev2010@gmail.com>
2019-12-14 10:26:54 +0500

    great!

---
+++ app/models/Logout.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+class Logout extends Database {
+    /*
+    *Выход пользователя из системы путем уничтожения сессии
+    */
+    public function userLogout(){
+        $_SESSION = array();
+    }
+
+
+}
